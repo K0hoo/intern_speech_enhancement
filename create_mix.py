f0e3cf7903ea0c250373c84af24cf63256ec3f8e
K0hoo <younghookwon@gm.gist.ac.kr>
2022-08-01 10:49:50 +0900

    create mix logarithm

--- create_mix.py
+++ create_mix.py
@@ -9,6 +9,8 @@ import torch
 import torchaudio
 from vis_sound import *
 
+CONST_LOG_LENGTH = 32000
+
 """
 To create noisy data, file system should be set like below.
 
@@ -59,16 +61,8 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train', action=argparse.BooleanOptionalAction, default=True)
     parser.add_argument('--vis_sound', action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument('--logarithm', '--log', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('--root_dataset_folder', type=str, required=True)
-    parser.add_argument('--output_seen_clean_folder', type=str, default='')
-    parser.add_argument('--output_unseen_clean_folder', type=str, default='')
-    parser.add_argument('--output_seen_noisy_folder', type=str, default='')
-    parser.add_argument('--output_unseen_noisy_folder', type=str, default='')
-    parser.add_argument('--output_seen_noise_folder', type=str, default='')
-    parser.add_argument('--output_unseen_noise_folder', type=str, default='')
-    parser.add_argument('--output_seen_dataset_csv', type=str, default='')
-    parser.add_argument('--output_unseen_dataset_csv', type=str, default='')
-    parser.add_argument('--output_log_txt', type=str, default='')
     parser.add_argument('--snr', type=float, default=10)
     args = parser.parse_args()
     return args
@@ -134,7 +128,14 @@ def vis_clean_sound_length(clean_folder):
         print(f"{second_lengths[sl]}")
 
 
-def save_data(name, amp, length, csv_writer, args, sample_rate=16000, b_seen=True):
+"""
+The data of clean, noise and noisy data is saved.
+'name' and 'output' parameters are dictionary type of which keys are 'clean', 'noise' and 'noisy'.
+They have the file names and path to save.
+'amp' and 'length' parameter are also dictionary type and of which keys are 'clean' and 'noise'.
+They do not have 'noisy' as a key because this function will create new noisy data.
+"""
+def save_data(name, amp, length, output, snr=10, sample_rate=16000):
 
     clean_name, noisy_name, noise_name = name['clean'], name['noisy'], name['noise']
     clean_amp, noise_amp = amp['clean'], amp['noise']
@@ -147,23 +148,14 @@ def save_data(name, amp, length, csv_writer, args, sample_rate=16000, b_seen=Tru
 
     split_noise_amp = noise_amp[:, start: start + clean_len]
     noise_rms = cal_rms(split_noise_amp)
-    adjusted_noise_rms = cal_adjusted_rms(clean_rms, args.snr)
+    adjusted_noise_rms = cal_adjusted_rms(clean_rms, snr)
 
     adjusted_noise_amp = split_noise_amp * (adjusted_noise_rms / noise_rms)
     noisy_amp = clean_amp + adjusted_noise_amp
-
-    if b_seen:
-        output_noisy_folder = args.output_seen_noisy_folder
-        output_clean_folder = args.output_seen_clean_folder
-        output_noise_folder = args.output_seen_noise_folder
-    else:
-        output_noisy_folder = args.output_unseen_noisy_folder
-        output_clean_folder = args.output_unseen_clean_folder
-        output_noise_folder = args.output_unseen_noise_folder
     
-    output_noisy_file = join(output_noisy_folder, noisy_name)
-    output_clean_file = join(output_clean_folder, clean_name)
-    output_noise_file = join(output_noise_folder, noise_name)
+    output_noisy_file = join(output['noisy'], noisy_name)
+    output_clean_file = join(output['clean'], clean_name)
+    output_noise_file = join(output['noise'], noise_name)
 
     torchaudio.save(
         output_noisy_file,
@@ -187,17 +179,33 @@ def save_data(name, amp, length, csv_writer, args, sample_rate=16000, b_seen=Tru
             sample_rate
         )
 
-    csv_writer.writerow([noisy_name, clean_name, noise_name])
 
-
+"""
+All of clean speech data for train and seen noise is followed with the above file system,
+and mixed by 'save_data' function.
+The logarithm condition in args is make the form of saved data different.
+The data will be saved with duration of 4 seconds and shorter data will be covered with zero padding with out logarithm.
+However, with logarithm, the data will be saved with duration of 2 seconds and shorter data will be ignored.
+Also, the logerdata will be used with every 2 seconds.
+"""
 def create_train_data(args):
 
     root_dataset_folder = args.root_dataset_folder
     clean_folder = join(join(root_dataset_folder, "TIMIT"), "TRAIN")
     noise_folder = join(join(root_dataset_folder, "NoiseX"), "seen")
 
-    log_file = open(args.output_log_txt, 'w', newline='')
-    csv_file = open(args.output_seen_dataset_csv, 'w', newline='')
+    b_log = args.logarithm
+    if b_log:
+        output_folder = join(join(root_dataset_folder, "data_log"), "train")
+    else:
+        output_folder = join(join(root_dataset_folder, "data"), "train")
+
+    output_noisy_folder = join(output_folder, "noisy")
+    output_clean_folder = join(output_folder, "clean")
+    output_noise_folder = join(output_folder, "noise")
+
+    log_file = open(join(output_folder, "mixing_log.txt"), 'w', newline='')
+    csv_file = open(join(output_folder, "dataset.csv"), 'w', newline='')
     csv_writer = csv.writer(csv_file)
 
     os.chdir(noise_folder)
@@ -208,47 +216,57 @@ def create_train_data(args):
         noise_len = noise_amp.size(1)
         clean_sub_folders = os.listdir(clean_folder)
         noise_idx = 0
+
         for clean_sub_folder in clean_sub_folders:
             clean_sub_sub_folders = os.listdir(join(clean_folder, clean_sub_folder))
+
             for clean_sub_sub_folder in clean_sub_sub_folders:
                 clean_cur_folder = join(clean_folder, clean_sub_folder)
                 clean_cur_folder = join(clean_cur_folder, clean_sub_sub_folder)
                 os.chdir(clean_cur_folder)
                 clean_files = glob.glob("*.WAV")
+
                 for clean_file in clean_files:
                     clean_amp, clean_sample_rate = torchaudio.load(join(clean_cur_folder, clean_file))
                     clean_len = clean_amp.size(1)
-                    noisy_name = '_'.join(
-                        (
-                            clean_sub_folder, 
-                            clean_sub_sub_folder, 
-                            clean_file.split('.')[0], 
-                            noise_file.split('.')[0]
+
+                    if b_log:
+                        for i in range(clean_len // CONST_LOG_LENGTH):
+                            noisy_name = '_'.join((clean_sub_folder, clean_sub_sub_folder, clean_file.split('.')[0], noise_file.split('.')[0], str(i))) + '.WAV'
+                            clean_name = '_'.join((clean_sub_folder, clean_sub_sub_folder, clean_file.split('.')[0], str(i))) + '.WAV'
+                            noise_name = '_'.join((noise_file.split('.')[0], str(noise_idx))) + '.WAV'
+                            save_data(
+                                name={'clean': clean_name, 'noisy': noisy_name, 'noise': noise_name},
+                                amp={'clean': clean_amp[:, i * CONST_LOG_LENGTH:(i + 1) * CONST_LOG_LENGTH], 'noise': noise_amp},
+                                length={'clean': CONST_LOG_LENGTH, 'noise': noise_len},
+                                output={
+                                    'noisy': output_noisy_folder,
+                                    "clean": output_clean_folder,
+                                    "noise": output_noise_folder
+                                },
+                                snr=args.snr,
+                                sample_rate=clean_sample_rate,
+                            )
+                            noise_idx += 1
+                            csv_writer.writerow([noisy_name, clean_name, noise_name])
+                    else:
+                        noisy_name = '_'.join((clean_sub_folder, clean_sub_sub_folder, clean_file.split('.')[0], noise_file.split('.')[0])) + '.WAV'
+                        clean_name = '_'.join((clean_sub_folder, clean_sub_sub_folder, clean_file))
+                        noise_name = '_'.join((noise_file.split('.')[0], str(noise_idx))) + '.WAV'
+                        save_data(
+                            name={'clean': clean_name, 'noisy': noisy_name, 'noise': noise_name},
+                            amp={'clean': clean_amp, 'noise': noise_amp},
+                            length={'clean': clean_len, 'noise': noise_len},
+                            output={
+                                'noisy': output_noisy_folder,
+                                'clean': output_clean_folder,
+                                'noise': output_noise_folder
+                            },
+                            snr=args.snr,
+                            sample_rate=clean_sample_rate,
                         )
-                    ) + '.WAV'
-                    clean_name = '_'.join(
-                        (
-                            clean_sub_folder,
-                            clean_sub_sub_folder,
-                            clean_file
-                        )
-                    )
-                    noise_name = '_'.join(
-                        (
-                            noise_file.split('.')[0],
-                            str(noise_idx)
-                        )
-                    ) + '.WAV'
-                    noise_idx += 1
-                    save_data(
-                        name={'clean': clean_name, 'noisy': noisy_name, 'noise': noise_name},
-                        amp={'clean': clean_amp, 'noise': noise_amp},
-                        length={'clean': clean_len, 'noise': noise_len},
-                        csv_writer=csv_writer,
-                        args=args,
-                        sample_rate=clean_sample_rate,
-                        b_seen=True
-                    )
+                        noise_idx += 1
+                        csv_writer.writerow([noisy_name, clean_name, noise_name])
 
                 print(f"Done. noise: {noise_file}, clean: {clean_sub_folder}/{clean_sub_sub_folder}")
                 log_file.write(f"Done. noise: {noise_file}, clean: {clean_sub_folder}/{clean_sub_sub_folder}\n")
@@ -256,7 +274,10 @@ def create_train_data(args):
     csv_file.close()
     log_file.close()
 
-
+"""
+Creating test data is similar to creating training data.
+The only difference is unseen noise. It will distinguish the seen and unseen noise.
+"""
 def create_test_data(args):
 
     root_dataset_folder = args.root_dataset_folder
@@ -264,67 +285,89 @@ def create_test_data(args):
     seen_noise_folder = join(join(root_dataset_folder, "NoiseX"), "seen")
     unseen_noise_folder = join(join(root_dataset_folder, "NoiseX"), "unseen")
 
-    log_file = open(args.output_log_txt, 'w', newline='')
-    seen_csv_file = open(args.output_seen_dataset_csv, 'w', newline='')
-    unseen_csv_file = open(args.output_unseen_dataset_csv, 'w', newline='')
-
-    seen_csv_writer = csv.writer(seen_csv_file)
-    unseen_csv_writer = csv.writer(unseen_csv_file)
+    b_log = args.logarithm
+    if b_log:
+        output_folder = join(join(root_dataset_folder, "data_log"), "test")
+    else:
+        output_folder = join(join(root_dataset_folder, "data"), "test")
 
     lb_seen = [True, False]
     for b_seen in lb_seen:
-        noise_folder = seen_noise_folder if b_seen else unseen_noise_folder
+        if b_seen:
+            noise_folder = seen_noise_folder
+            su_output_folder = join(output_folder, "seen")
+        else:
+            noise_folder = unseen_noise_folder
+            su_output_folder = join(output_folder, "unseen")
+        output_noisy_folder = join(su_output_folder, "noisy")
+        output_clean_folder = join(su_output_folder, "clean")
+        output_noise_folder = join(su_output_folder, "noise")
+        csv_file = open(join(su_output_folder, "dataset.csv"), 'w', newline='')
+        log_file = open(join(su_output_folder, "log.txt"), 'a', newline='')
+        csv_writer = csv.writer(csv_file)
+
         os.chdir(noise_folder)
         noise_files = glob.glob("*.wav")
-        for i, noise_file in enumerate(noise_files):
+
+        for noise_file in noise_files:
             noise_amp, _ = torchaudio.load(join(noise_folder, noise_file))
             noise_amp =  torch.repeat_interleave(noise_amp, 2, dim=1)
             noise_len = noise_amp.size(1)
             clean_sub_folders = os.listdir(clean_folder)
             noise_idx = 0
+
             for clean_sub_folder in clean_sub_folders:
                 clean_sub_sub_folders = os.listdir(join(clean_folder, clean_sub_folder))
+
                 for clean_sub_sub_folder in clean_sub_sub_folders:
                     clean_cur_folder = join(clean_folder, clean_sub_folder)
                     clean_cur_folder = join(clean_cur_folder, clean_sub_sub_folder)
                     os.chdir(clean_cur_folder)
                     clean_files = glob.glob("*.WAV")
+
                     for clean_file in clean_files:
                         clean_amp, clean_sample_rate = torchaudio.load(join(clean_cur_folder, clean_file))
                         clean_len = clean_amp.size(1)
-                        noisy_name = '_'.join(
-                            (
-                                clean_sub_folder,
-                                clean_sub_sub_folder,
-                                clean_file.split('.')[0],
-                                noise_file.split('.')[0]
-                            )
-                        ) + ".WAV"
-                        clean_name = '_'.join(
-                            (
-                                clean_sub_folder,
-                                clean_sub_sub_folder,
-                                clean_file
-                            )
-                        )
-                        noise_name = '_'.join(
-                            (
-                                noise_file.split('.')[0],
-                                str(noise_idx)
-                            )
-                        ) + '.WAV'
 
-                        noise_idx += 1
+                        if b_log:
+                            for i in range(clean_len // CONST_LOG_LENGTH):
+                                noisy_name = '_'.join((clean_sub_folder, clean_sub_sub_folder, clean_file.split('.')[0], noise_file.split('.')[0], str(i))) + ".WAV"
+                                clean_name = '_'.join((clean_sub_folder, clean_sub_sub_folder, clean_file.split('.')[0], str(i))) + '.WAV'
+                                noise_name = '_'.join((noise_file.split('.')[0], str(noise_idx))) + '.WAV'
 
-                        save_data(
-                            name={'clean': clean_name, 'noisy': noisy_name, 'noise': noise_name},
-                            amp={'clean': clean_amp, 'noise': noise_amp},
-                            length={'clean': clean_len, 'noise': noise_len},
-                            csv_writer= seen_csv_writer if b_seen else unseen_csv_writer,
-                            args=args,
-                            sample_rate=clean_sample_rate,
-                            b_seen=b_seen
-                        )
+                                save_data(
+                                    name={'clean': clean_name, 'noisy': noisy_name, 'noise': noise_name},
+                                    amp={'clean': clean_amp[:, i * CONST_LOG_LENGTH:(i + 1) * CONST_LOG_LENGTH], 'noise': noise_amp},
+                                    length={'clean': CONST_LOG_LENGTH, 'noise': noise_len},
+                                    output={
+                                        'noisy': output_noisy_folder,
+                                        'clean': output_clean_folder,
+                                        'noise': output_noise_folder
+                                    },
+                                    snr=args.snr,
+                                    sample_rate=clean_sample_rate,
+                                )
+                                noise_idx += 1
+                                csv_writer.writerow([noisy_name, clean_name, noise_name])
+                        else:
+                            noisy_name = '_'.join((clean_sub_folder, clean_sub_sub_folder, clean_file.split('.')[0], noise_file.split('.')[0], str(i))) + ".WAV"
+                            clean_name = '_'.join((clean_sub_folder, clean_sub_sub_folder, clean_file.split('.')[0], str(i))) + '.WAV'
+                            noise_name = '_'.join((noise_file.split('.')[0], str(noise_idx))) + '.WAV'
+
+                            save_data(
+                                name={'clean': clean_name, 'noisy': noisy_name, 'noise': noise_name},
+                                amp={'clean': clean_amp, 'noise': noise_amp},
+                                length={'clean': clean_len, 'noise': noise_len},
+                                output={
+                                    'noisy': output_noisy_folder,
+                                    'clean': output_clean_folder,
+                                    'noise': output_noise_folder
+                                },
+                                snr=args.snr,
+                                sample_rate=clean_sample_rate,
+                            )
+                            noise_idx += 1
+                            csv_writer.writerow([noisy_name, clean_name, noise_name])
                     
                     if b_seen:
                         print(f"Done. seen noise: {noise_file}, clean: {clean_sub_folder}/{clean_sub_sub_folder}")
@@ -332,10 +375,9 @@ def create_test_data(args):
                     else:
                         print(f"Done. unseen noise: {noise_file}, clean: {clean_sub_folder}/{clean_sub_sub_folder}")
                         log_file.write(f"Done. unseen noise: {noise_file}, clean: {clean_sub_folder}/{clean_sub_sub_folder}\n")
-
-    seen_csv_file.close()
-    unseen_csv_file.close()
-    log_file.close()
+        
+        csv_file.close()
+        log_file.close()
 
 
 if __name__ == '__main__':
