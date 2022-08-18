bafaae4cb8a60b6db0cb00f4c3f839127ac60a8d
K0hoo <younghookwon@gm.gist.ac.kr>
2022-08-18 13:41:45 +0900

    TCN model and file

---
+++ models_TCN.py
@@ -0,0 +1,82 @@
+
+from numpy import transpose
+import torch
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+
+
+# Baseline for TCN model from https://github.com/locuslab/TCN/blob/master/TCN/tcn.py
+# modified for non-causal and no dropout
+
+
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, self.chomp_size:-self.chomp_size].contiguous()
+
+
+class TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding//2)
+        self.relu1 = nn.PReLU()
+
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding//2)
+        self.relu2 = nn.PReLU()
+
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1,
+                                 self.conv2, self.chomp2, self.relu2,)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.PReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res) # residual
+
+
+class TemporalConvNet(nn.Module):
+    def __init__(self, num_inputs, num_channels, kernel_size=2):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size)]
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+
+# Model
+
+class TCN(nn.Module):
+    def __init__(self, input_size, output_size, num_channels, kernel_size):
+        super(TCN, self).__init__()
+        self.tcn1 = TemporalConvNet(input_size, num_channels, kernel_size)
+        self.tcn2 = TemporalConvNet(input_size, num_channels, kernel_size)
+        self.tcn3 = TemporalConvNet(input_size, num_channels, kernel_size)
+
+    def forward(self, x):
+        output = self.tcn1(x.transpose(1, 2))
+        output = self.tcn2(output)
+        return self.tcn3(output).transpose(1, 2)
